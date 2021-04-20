57c35bfcf6aa3cd8f71a366781456f32d0009e98
poix-code <1996@holbertonschool.com>
2021-04-20 13:52:47 -0500

    Let variable testing

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-document.write("<h1>Hello JavaScript</h1>")
+document.write("<h1>Hello JavaScript</h1>");
+
+let passwd = '123uwu';
+let inputTest = '123uwu'
+
+let check = inputTest === passwd;
+
+if (check == true) {console.log('Correct password')}
+
+if (check == false) {console.log('Incorrect password')}
